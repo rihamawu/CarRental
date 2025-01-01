6ed0b791b59639b7102a3028d7980e6308fec45e
riri <rihambaquroun@gmail.com>
2025-01-01 15:32:38 +0100

    first

--- CarRental/Program.cs
+++ CarRental/Program.cs
@@ -2,9 +2,16 @@ using CarRental.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<CarRentalContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CarRentalDb")));
-// Add services to the container.
-builder.Services.AddRazorPages();
+
+// Add DbContext configuration
+builder.Services.AddDbContext<CarRentalContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CarRentalDb")));
+
+// Add services for MVC (controllers with views)
+builder.Services.AddControllersWithViews();
+
+// Add services for Razor Pages (if you are using Razor Pages)
+builder.Services.AddRazorPages(); // This was missing in your code
 
 var app = builder.Build();
 
@@ -13,12 +20,21 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+
 app.UseStaticFiles();
 
+// Enable routing
 app.UseRouting();
 
+// Enable authorization
 app.UseAuthorization();
 
+// Map controller routes
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Optionally, if you are also using Razor Pages:
 app.MapRazorPages();
 
 app.Run();
